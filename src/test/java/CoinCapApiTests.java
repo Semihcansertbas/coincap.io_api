115a9f6816c4a63ab40344a42548246673532276
Semihcan Sertbas <40262346+Semihcansertbas@users.noreply.github.com>
2024-01-29 14:55:28 -0600

    Update CoinCapApiTests.java

--- src/test/java/CoinCapApiTests.java
+++ src/test/java/CoinCapApiTests.java
@@ -1,17 +1,13 @@
-
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import io.qameta.allure.*;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 
 @Epic("CoinCap API Tests")
-@Feature("API Test Cases")
+@Feature("CoinCap High Level API Test Cases")
 public class CoinCapApiTests {
 
 
@@ -19,85 +15,158 @@ public class CoinCapApiTests {
         RestAssured.baseURI = "https://api.coincap.io/v2"; // Set your base URI here
     }
 
-
     @Test
-    @Description("Retrieve List of Coins")
-    public void testGetCoins() {
-        given()
+    @Link(name = "coincap.io", url = "https://coincap.io/")
+    @Description("Verify Response Schema for List of Assets")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Make a GET request to retrieve a list of assets")
+    public void testListAssetsResponseSchema() {
+        // Perform API request
+        String response = given()
                 .when()
                 .get("/assets")
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", not(empty()));
-
+                .body("data", not(empty()))
+                .extract().asString();
+        // Attach API response to Allure report
+        attachApiResponseToAllure(response);
     }
 
     @Test
-    @Description("Invalid Endpoint")
-    public void testInvalidEndpoint() {
-        given()
+    @Link(name = "coincap.io", url = "https://coincap.io/")
+    @Description("Verify Response Schema for Single Asset")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Make a GET request to retrieve details for a specific asset")
+    public void testSingleAssetResponseSchema() {
+        String validId = "bitcoin";
+        // Perform API request with path parameter
+        String response = given()
                 .when()
-                .get("/coinsList")
-                .then()
-                .statusCode(404)
-                .contentType(ContentType.JSON)
-                .body("error", equalTo("Not Found"));
-    }
-
-    @Test
-    @Description("Retrieve Specific Coin Information")
-    public void testGetCoinById() {
-        String validCoinId = "bitcoin";
-
-        given()
-                .pathParam("coin_id", validCoinId)
-                .when()
-                .get("/coins/{coin_id}")
+                .pathParam("id", validId)
+                .get("assets/{id}")
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data.id", equalTo(validCoinId));
+                .body("data.id", equalTo(validId))
+                .body("data", not(empty()))
+                .extract().asString();
+        // Attach API response to Allure report
+        attachApiResponseToAllure(response);
     }
 
     @Test
-    @Description("Invalid Coin ID")
-    public void testInvalidCoinId() {
-        String invalidCoinId = "inv_id";
-
-        given()
-                .pathParam("coin_id", invalidCoinId)
+    @Link(name = "coincap.io", url = "https://coincap.io/")
+    @Description("Verify Historical Data Endpoint")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Make a GET request to retrieve historical data for a specific asset")
+    public void testHistoricalDataEndpoint() {
+        String validId = "bitcoin";
+        // Perform API request with path parameter
+        String response = given()
+                .pathParam("id", validId)
                 .when()
-                .get("/coins/{coin_id}")
-                .then()
-                .statusCode(404)
-                .contentType(ContentType.JSON)
-                .body("error", equalTo("Not Found"));
-    }
-
-    @Test
-    @Description("Retrieve Price Information")
-    public void testGetPrices() {
-        given()
-                .queryParam("ids", "bitcoin,ethereum")
-                .when()
-                .get("/prices")
+                .get("assets/{id}/history?interval=d1")
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", not(empty()));
+                .body("data", not(empty()))
+                .extract().asString();
+        // Attach API response to Allure report
+        attachApiResponseToAllure(response);
     }
 
     @Test
-    @Description("Missing Parameters")
-    public void testMissingParameters() {
-        given()
+    @Link(name = "coincap.io", url = "https://coincap.io/")
+    @Description("Verify Sorting by Market Capital")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Make a GET request to retrieve sorting by Market Capital")
+    public void testSortingByMarketCap() {
+        String validId = "bitcoin";
+        // Perform API request with path parameter
+        String response = given()
+                .pathParam("id", validId)
                 .when()
-                .get("/prices")
+                .get("/assets/{id}/markets")
                 .then()
-                .statusCode(400)
+                .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("error", equalTo("Bad Request"));
+                .body("data", not(empty()))
+                .extract().asString();
+        // Attach API response to Allure report
+        attachApiResponseToAllure(response);
     }
+
+
+    @Test
+    @Link(name = "coincap.io", url = "https://coincap.io/")
+    @Description("Verify Response Schema for Assets Rates")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Make a GET request to retrieve a list of assets rates")
+    public void verifyAssetsRatesResponseSchema() {
+        String response = given()
+                .when()
+                .get("/rates")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", not(empty()))
+                .extract().asString();
+        // Attach API response to Allure report
+        attachApiResponseToAllure(response);
+    }
+
+    @Test
+    @Link(name = "coincap.io", url = "https://coincap.io/")
+    @Description("Verify Response Schema for Single Asset Rates")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Make a GET request to retrieve details for a specific asset rates")
+    public void verifySingleAssetRatesResponseSchema() {
+        String validId = "ethereum";
+        // Perform API request with path parameter
+        String response = given()
+                .pathParam("id", validId)
+                .when()
+                .get("/rates/{id}")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", not(empty()))
+                .extract().asString();
+        // Attach API response to Allure report
+        attachApiResponseToAllure(response);
+    }
+
+    @Test
+    @Link(name = "coincap.io", url = "https://coincap.io/")
+    @Description("Fetch Asset History with Invalid Symbol")
+    @Severity(SeverityLevel.CRITICAL)
+    @Step("Make a GET request to retrieve Error unable to find Asset")
+    public void fetchAssetHistoryWithInvalidSymbol() {
+        String invalidSymbol = ">"; // Replace with an invalid or non-existent symbol
+        // Perform API request with path parameter
+        String response = given()
+                .pathParam("symbol", invalidSymbol)
+                .queryParam("interval", "d1")
+                .when()
+                .get("/assets/{symbol}/history")
+                .then()
+                .statusCode(404)
+                .contentType(ContentType.JSON)
+                .body("data", not(empty()))
+                .extract().asString();
+        // Attach API response to Allure report
+        attachApiResponseToAllure(response);
+    }
+
+
+
+            @Step("Attach API response to Allure report")
+            private void attachApiResponseToAllure(String response) {
+                Allure.addAttachment("API Response", "application/json", response, "json");
+            }
+
+
 
 }
